8c64a4a9852f896e886bb57d615816a668821c7f
KenShynn <magalonakaj@gmail.com>
2021-12-19 11:57:52 +0800

    kyaaaaaa

---
+++ PyEqual_And_Equivalent.py
@@ -0,0 +1,23 @@
+#King Aj C. Magalona
+# A = {1, 2, 3, 4, 5, 6, 7, 8}
+# B = {1, 2, 5, 7}
+
+# A = {2, 4, 6}
+# B = {2, 4, 6}
+# King Aj C. Magalona
+A = {1, 5, 6, 4, 9, 2, 0}
+B = {5, 4, 9, 6, 2} #My Inputs
+
+A = {1, 2, 3, 4, 5}; 
+B = {1, 2, 6,};  #My Inputs
+
+print(f"Python code\n")
+print(f"Set A \t\t\t\t:{A}")
+print(f"Set B \t\t\t\t:{B}\n")
+
+print(f"Cardinality of A is \t\t: {len(A)}")
+print(f"Cardinality of B is \t\t: {len(B)}")
+print(f"B is a subset of A \t\t: {B <= A}")
+print(f"B is a proper subset of A \t: {B < A}")
+print(f"\nKing Aj C. Magalona")
+# King Aj C. Magalona
---
+++ PySubsets.py
@@ -0,0 +1,20 @@
+# King Aj Magalona
+# A = {1, 3, 5, 7, 9}
+# B = {3, 5, 1, 9, 7}
+
+# A = {1, 3, 5, 7, 9}
+# B = {2, 4, 1, 9, 7}
+
+A = {1, 3, 5, 7, 9}
+B = {3, 5, 1, 9}    #Testing Sets
+
+print("Python code\n")
+print(f"Set A \t\t\t:{A}")
+print(f"Set B \t\t\t:{B}\n")
+
+print(f"Cardinality of A is\t:{len(A)}")
+print(f"Cardinality of A is\t:{len(B)}")
+
+print(f"Equal Set\t\t: {A == B}")
+print(f"Equivalent Set\t\t: {len(A) == len(B)}")
+print(f"\nKing Aj C. Magalona")
